36d73c4fae414f7f7f757173d491c79796eb5211
grubbywww <wangyj@easemob.com>
2017-03-20 23:13:55 +0800

    publish

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Role(db.Model):
     @staticmethod
     def insert_roles():
         roles = {
-            'Exector': Permission.CREATE_TASK|Permission.VERIFY_TASK|Permission.RELEASE_TASK
+            'Exector': int(Permission.CREATE_TASK|Permission.VERIFY_TASK|Permission.RELEASE_TASK)
         }
         for r in roles:
             role = Role.query.filter_by(name=r).first()
