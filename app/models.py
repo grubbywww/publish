2175e02df2c72b12f2009f2dacedbebf94a68fdd
grubbywww <wangyj@easemob.com>
2017-03-21 13:13:30 +0800

    ROLE

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class Role(db.Model):
             'Exector': (Permission.CREATE_TASK|Permission.RELEASE_TASK|Permission.CHECK_TASK),
             'Auditor': (Permission.CREATE_TASK|Permission.VERIFY_TASK|Permission.RELEASE_TASK|Permission.CHECK_TASK),
             'Observer': (CHECK_TASK),
-            'Admin': ((Permission.CREATE_OPERATOR|Permission.CHECK_RESOURCE|Permission.APPLICATION_RESOURCE|Permission.CREATE_APPLICATION)
+            'Admin': (Permission.CREATE_OPERATOR|Permission.CHECK_RESOURCE|Permission.APPLICATION_RESOURCE|Permission.CREATE_APPLICATION)
         }
         for r in roles:
             role = Role.query.filter_by(name=r).first()
