84e04608d17974de59bc26aa3ecaea63562c060f
grubbywww <wangyj@easemob.com>
2017-03-28 17:21:31 +0800

    p

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,7 +10,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember.data)
-            response = make_response()
+            response = make_response(render_template('auth/login.html')
             response.set_cookie('cname','test')
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password !')
